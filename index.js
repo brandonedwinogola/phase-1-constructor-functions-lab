06b643b86f3c9309ef47617f93ff78e655cce183
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-19 15:56:52 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// constructor function
+function user(year, colour) {
+    this.year = year;
+    this.colour = co;
+    our
+    this.model = function() {
+        console.log(`Swoosh,my year of produce is ${this.year} my colour is ${this.colour}!`);
+    };
+}
+
+const swoosh = new user("Swoosh", "2015", "red")
+    // => Swoosh,2015,red
+
+function user(name, age, experience)
+this.name = name;
+this.age = age;
+this.experiencev = experience;
+console.log(`this is my name ${this.name} this is my age ${this.age} and i have this experince ${this.experience}
+          `)
+
+// Allison,16 years,2 years
+
+
+function user(address, city)
+this.address = address;
+this.city = city;
+console.log(`this is my address ${this.addrss} and my city ${this.city}`)
+
+// 123,Broadway
